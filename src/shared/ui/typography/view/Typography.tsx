264893e3d6efe32cd96a95d44a82b4e8b0f62d00
zik30 <ainazik.ishenbek@gmail.com>
2025-07-02 22:33:27 +0600

    test

---
+++ src/shared/ui/typography/view/Typography.tsx
@@ -0,0 +1,15 @@
+import { type FC } from 'react'
+
+export const Typography:FC = ({
+    variant, 
+    align, 
+    weight, 
+    color, 
+    children, 
+    className
+}) => {
+  return (
+    <div>:FC</div>
+  )
+}
+
