b29de457bce3578088983dc1a2773ffe4da23fac
Amin Soltobekov <1kkro123@gmail.com>
2025-07-02 18:30:54 +0600

    some

--- eslint.config.js
+++ eslint.config.js
@@ -6,24 +6,19 @@ import tseslint from 'typescript-eslint';
 import reactRecommended from 'eslint-plugin-react/configs/recommended.js';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      reactRecommended,
-    ],
+    ignores: ['dist', 'node_modules'],
+  },
+  {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 'latest',
       globals: {
         ...globals.browser,
         ...globals.node,
       },
       parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
+        ecmaFeatures: { jsx: true },
         sourceType: 'module',
       },
     },
@@ -31,6 +26,11 @@ export default tseslint.config(
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      reactRecommended,
+    ],
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
@@ -41,7 +41,6 @@ export default tseslint.config(
       'react/jsx-uses-react': 'off',
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
-      '@typescript-eslint/explicit-function-return-type': 'off',
       '@typescript-eslint/no-unused-vars': 'error',
       '@typescript-eslint/no-explicit-any': 'warn',
       'no-console': 'error',
