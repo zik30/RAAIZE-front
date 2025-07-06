cc603ff9f21b809c6dd3d7c0b46fdcef89fc1df5
Amin Soltobekov <1kkro123@gmail.com>
2025-07-06 18:17:49 +0600

    faq and price pages are done

---
+++ src/widgets/pricing/view/Pricing.tsx
@@ -0,0 +1,86 @@
+import { Container, CustomButton, Typography } from '@src/shared/ui';
+import styles from './Pricing.module.scss';
+import { PricingPlan } from '../types/types';
+import { pricingPlans } from '@src/shared/constants/constants';
+import { Check } from 'lucide-react';
+
+const PricingCard = ({ plan }: { plan: PricingPlan }) => (
+  <div className={styles.card}>
+    <div className={styles.title}>
+      <Typography variant="h3" color="white" weight="medium">
+        {plan.title}
+      </Typography>
+      {plan.isPopular && (
+        <div className={styles.isPopular}>
+          <Typography variant="extraSmall" color="lightBlue" weight="bold">
+            POPULAR
+          </Typography>
+        </div>
+      )}
+    </div>
+
+    <div className={styles.price}>
+      <Typography variant="h2" color="white">
+        {plan.price}
+        <span className={styles.smallText}> {plan.period}</span>
+      </Typography>
+    </div>
+
+    <div className={styles.creditInfo}>
+      <Typography variant="smallText" color="grey">
+        {plan.description}
+      </Typography>
+      {plan.credit && (
+        <div className={styles.creditAmount}>
+          <Typography variant="smallText" color="grey">
+            {plan.credit}
+          </Typography>
+        </div>
+      )}
+      {plan.buttonText && (
+        <CustomButton color="secondary" size="small" disabled>
+          {plan.buttonText}
+        </CustomButton>
+      )}
+    </div>
+
+    <div className={styles.features}>
+      <Typography variant="smallText" color="white">
+        Specifications:
+      </Typography>
+      <ul className={styles.featuresList}>
+        {plan.features.map((feature, index) => (
+          <li key={index} className={styles.listElement}>
+            <Check color="white" size={17} />
+            <Typography variant="smallText" color="grey">
+              {feature}
+            </Typography>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
+);
+
+export const Pricing = () => {
+  return (
+    <Container>
+      <div className={styles.pricingBlock}>
+        <div className={styles.textArea}>
+          <Typography variant="h2" color="white" weight="bold">
+            Pricing
+          </Typography>
+          <Typography variant="bodyText" color="grey">
+            Start for free. Upgrade to get the capacity that exactly matches
+            your team&apos;s needs.
+          </Typography>
+        </div>
+        <div className={styles.cardsList}>
+          {pricingPlans.map((plan, index) => (
+            <PricingCard key={index} plan={plan} />
+          ))}
+        </div>
+      </div>
+    </Container>
+  );
+};
